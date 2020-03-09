317b019c43adf8bc76caa940c5a0762081b73a91
Ignacio - Innovo <ignacioelfi@gmail.com>
2020-03-09 19:57:41 -0300

    Initial version.

---
+++ MDBEcommerce/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MDBEcommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.RenderCarousel = true;
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            ViewBag.RenderCarousel = false;
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            ViewBag.RenderCarousel = false;
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# MDBEcommerce
-ASP.NET MVC MDBootstrap Ecommerce Template
+# MDB Ecommerce Template in ASP.NET MVC
+
+Integrating MDBootstrap in ASP.NET MVC
+
+Here you will find a working project to take as starting point.
+
+Thanks to the MDBootstrap Team.
